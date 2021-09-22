8f91e793bb939f79663b9fe5efdcaff2a5f71fba
Munkh-Undral E <1236emu@gmail.com>
2021-09-22 10:03:22 -0500

    bugfix

--- src/main/java/edu/miu/waa/ministore/MiniStoreApplication.java
+++ src/main/java/edu/miu/waa/ministore/MiniStoreApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MaskmStoreApplication {
+public class MiniStoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MaskmStoreApplication.class, args);
+        SpringApplication.run(MiniStoreApplication.class, args);
     }
 
 }
