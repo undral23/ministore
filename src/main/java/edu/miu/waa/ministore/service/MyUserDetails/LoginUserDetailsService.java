188b50da528bea5d1bac2315cbfcac9436ee732b
Munkh-Undral E <1236emu@gmail.com>
2021-09-23 10:13:28 -0500

    changes

--- src/main/java/edu/miu/waa/ministore/service/MyUserDetails/LoginUserDetailsService.java
+++ src/main/java/edu/miu/waa/ministore/service/MyUserDetails/LoginUserDetailsService.java
@@ -1,29 +1,28 @@
 package edu.miu.waa.ministore.service.MyUserDetails;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
 import edu.miu.waa.ministore.domain.User;
 import edu.miu.waa.ministore.repository.UserRepository;
 
-import java.util.Optional;
-
 @EnableWebSecurity
 public class LoginUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    UserRepository userRepository;
+	@Autowired
+	UserRepository userRepository;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        Optional<User> user = userRepository.findUserByUsername(username);
-        user.orElseThrow(()-> new UsernameNotFoundException("USER NOT FOUND"));
+		Optional<User> user = userRepository.findUserByUsername(username);
+		user.orElseThrow(() -> new UsernameNotFoundException("USER NOT FOUND"));
 
-        return new LoginUserDetails(user.get());
-    }
+		return new LoginUserDetails(user.get());
+	}
 }
